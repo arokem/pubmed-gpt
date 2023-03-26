7108f4927e0584ea6701765debc937409ac904c9
Ariel Rokem <arokem@gmail.com>
2023-03-25 21:32:49 -0700

    Use env variables if those are set.

--- pmgpt/pmgpt.py
+++ pmgpt/pmgpt.py
@@ -1,5 +1,6 @@
 from typing import Optional
 import click
+import uuid
 
 from pymed import PubMed
 import openai
@@ -9,7 +10,7 @@ def query_pubmed(topic: str, email: str, max_results: Optional[int]=5) -> str:
     """
     Get abstracts for a topic from pubmed
     """
-    pubmed = PubMed(tool="pubmed-gpt", email=email)
+    pubmed = PubMed(tool=f"pubmed-gpt-{uuid.uuid1()}", email=email)
     pm_results = list(pubmed.query(topic, max_results=max_results))
     abstracts = """ \n""".join([
     f"""Title: {aa.title}
@@ -59,23 +60,21 @@ def query_gpt(
 @click.option('--topic', prompt="Please enter topic to query",
               help='Topic to query')
 @click.option('--email',
-              prompt='Please enter your email address (pubmed requires this)',
+              envvar='EMAIL',
               help='Email address is required to query pubmed')
 @click.option('--apikey',
-              prompt='Please enter your OpenAI API key (skip to use environment variable)',
+              default=None,
+              envvar='OPENAI_API_KEY',
               help='OpenAI API key is required to query GPT')
 def main(topic: str,
          email: str,
-         apikey: Optional[str|None]=None,
+         apikey: str,
          model: Optional[str]="gpt-3.5-turbo",
          tofile: Optional[str]="presentation.md"):
 
-    if apikey is not None:
-        openai.api_key = apikey
-    # Otherwise we assume that it's set as an environment variable.
-
     abstracts = query_pubmed(topic, email)
 
+    openai.api_key = apikey
     response = query_gpt(
         topic,
         abstracts,
